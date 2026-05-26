eebf4416865ad55ee89d0fdf6e538717f7c2e52b
Jossie-riko <jossie.ricob@uniagustiniana.edu.co>
2026-05-26 01:40:21 +0000

    a

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,7 +5,6 @@ import SWRegistration from "./SWRegistration";
 export const metadata = {
   title: "Libres PWA",
   description: "Contador de partidas libres de billar",
-  manifest: "/manifest.json",
 };
 
 export default function RootLayout({
@@ -13,13 +12,13 @@ export default function RootLayout({
 }: {
   children: React.ReactNode;
 }) {
-
   return (
     <html lang="es">
+      <head>
+        <link rel="manifest" href="/manifest.json" />
+      </head>
       <body>
-
         <SWRegistration />
-
         <nav
           style={{
             display: "flex",
@@ -28,25 +27,10 @@ export default function RootLayout({
             background: "black"
           }}
         >
-
-          <Link
-            href="/"
-            style={{ color: "white" }}
-          >
-            Inicio
-          </Link>
-
-          <Link
-            href="/reglas"
-            style={{ color: "white" }}
-          >
-            Reglas
-          </Link>
-
+          <Link href="/" style={{ color: "white" }}>Inicio</Link>
+          <Link href="/reglas" style={{ color: "white" }}>Reglas</Link>
         </nav>
-
         {children}
-
       </body>
     </html>
   );
